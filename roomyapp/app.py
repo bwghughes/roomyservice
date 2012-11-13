bfa9b674b8d79f21620c485a6792e3b699d85126
Ben Hughes <bwghughes@gmail.com>
2012-11-13 10:17:27 +0000

    Flask classy refactor.

--- roomyapp/app.py
+++ roomyapp/app.py
@@ -1,59 +1,11 @@
-#!/usr/bin/env python
-import json
+from flask import Flask
+from api import ApiView
 from logbook import Logger
-from flask import Flask, request, jsonify, abort, make_response
-from schematics.base import TypeException
-
-from models.events import CameraEvent
-from models.device import CameraDevice
-from models.store import EventStore, DeviceStore
-
 
 app = Flask(__name__)
 log = Logger(__name__)
-__version__ = "0.1"
 
-event_store = EventStore()
-device_store = DeviceStore()
-
-
-@app.route("/")
-def index():
-    return jsonify(dict(version=__version__))
-
-
-@app.route("/device/register", methods=['POST'])
-def register_device():
-    device = CameraDevice(**json.loads(request.data))
-    try:
-        device.validate()
-        device_store.add(device)
-        response = make_response()
-        response.headers = dict(content_type="application/json")
-        response.data = device.make_json_publicsafe(device)
-        return response
-    except TypeException, e:
-        print e
-        abort(405, 'Invalid payload')
-
-
-@app.route("/event/camera/<device_id>", methods=["POST"])
-def camera_event(device_id):
-    camera_event = CameraEvent(**json.loads(request.data))
-    if device_id in device_store.devices():
-        try:
-            # Now we need to set it
-            camera_event.device_id = device_id
-            camera_event.validate()
-            event_store.add(camera_event)
-            return jsonify({'status': 'OK'})
-        except ShieldException, e:
-            print e
-            log.error("Invalid payload - {}".format(e))
-            abort(405, 'Invalid payload')
-    else:
-        abort(404)
-
+ApiView.register(app, route_base='/')
 
 if __name__ == "__main__":
     app.run(debug=True)
